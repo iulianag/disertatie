555906e3995f6336dfc0946618b257b98a9d31ca
Iuliana Gabor <gabor_iuliana@yahoo.com>
2020-08-04 00:25:05 +0300

    Added login and logout endpoints for bussines_logic

--- business_logic/settings.py
+++ business_logic/settings.py
@@ -4,4 +4,8 @@ from fastapi import FastAPI
 SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 5002
 
+DB_SERVER_HOST = "localhost"
+DB_SERVER_PORT = 5001
+DB_SERVER_URL = f"http://{DB_SERVER_HOST}:{DB_SERVER_PORT}"
+
 app = FastAPI()
--- business_logic/src/endpoints/authorization.py
+++ business_logic/src/endpoints/authorization.py
@@ -1,25 +1,37 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Security
+from fastapi.security import APIKeyHeader
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
+import requests
 
+from settings import DB_SERVER_URL
 from src.utils.base_utils import raise_exception
+from src.validation_models.user_model import UserCredentialsIn
 
 router = APIRouter()
 
 
 @router.post("/login",
              tags=["authorization"])
-async def login():
+async def login(item: UserCredentialsIn):
     try:
-        return {"test": "fake result"}
+        response = requests.post(f"{DB_SERVER_URL}/login", json=item.dict())
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=response.json()
+        )
     except Exception as e:
         raise_exception(e)
 
 
 @router.post("/logout",
              tags=["authorization"])
-async def logout():
+async def logout(authorization=Security(APIKeyHeader(name="Authorization", auto_error=False))):
     try:
-        return {"test": "fake result"}
+        response = requests.post(f"{DB_SERVER_URL}/logout", headers={"Authorization": authorization})
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=response.json()
+        )
     except Exception as e:
         raise_exception(e)
